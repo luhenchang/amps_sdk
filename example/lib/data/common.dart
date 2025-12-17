8555447669187d6f2b526df28add3c62ab312840
luhenchang <1302023572@qq.com>
2025-12-17 21:00:56 +0800

    鸿蒙端更新sdk2 1、点击自渲染问题flutter支持 2、删除文件

--- example/lib/data/common.dart
+++ example/lib/data/common.dart
@@ -5,5 +5,5 @@ var appId = Platform.isIOS ? "12391" : "12379";
 var splashSpaceId = Platform.isIOS ? '15285' : "15288";
 var interstitialSpaceId = Platform.isIOS ? '111499' : "111502";
 var nativeSpaceId = Platform.isIOS ? '15292' : "15295";
-var unifiedSpaceId = "118007";///Platform.isIOS ? '124302' : "124302";
+var unifiedSpaceId = Platform.isIOS ? '124302' : "124302";
 var timeOut = 5000;//广告请求超时时长，建议5000毫秒,该参数单位为ms
